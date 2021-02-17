8aad4c33841517641829f6aaf3df76fecd93efb6
droid4alex <droid4alex@gmail.com>
2021-02-17 03:02:09 -0800

    Adds User Auth: CSS for nav and signup links

--- frontend/components/projects/projects.jsx
+++ frontend/components/projects/projects.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Projects = ({ currentUser, logout }) => {
   const demoboard = () => (
-    <div>
-      <center>
-        <h1>D E M O</h1>
-      </center>
+    <div className="grid grid--centered grid--formatted centered-medium">
+      <h1 className="heading heading--xx-large grid__item grid__item--large">Work better, together.</h1>
+      <p className="grid__item grid__item--medium flush--bottom">Basecamp is more than just a project management tool — it’s a better way to work. Teams that switch to Basecamp are more productive and better organized. They communicate better and require fewer meetings. And they’re far more efficient than before. Here’s how Basecamp makes it happen.</p>
     </div>
   );
   const dashboard = () => (
