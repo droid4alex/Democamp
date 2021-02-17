a9e19a54f7a4a5fdeef51b8212ff72686d902d2e
droid4alex <droid4alex@gmail.com>
2021-02-17 15:17:11 -0800

    Tweaks User Auth: CSS

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -105,9 +105,9 @@ class SessionForm extends React.Component {
     return (
       <div className="login-body">
         <div className="login-logo">
-          <a className="login-nav__logo" href="/" aria-label="Basecamp">
-            <svg viewBox="0 0 100 60" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="basecamp-logo-title">
-              <title id="basecamp-logo-title">Democamp logo</title>
+          <a className="login-nav__logo" href="/" aria-label="Democamp">
+            <svg viewBox="0 0 100 60" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="democamp-logo-title">
+              <title id="democamp-logo-title">Democamp logo</title>
               <path className="icon" d="M 60.64 41.12 c -1.44 0 -2.56 -1.12 -2.72 -2.56 c -0.96 -13.76 -8.48 -33.12 -22.4 -33.12 c -11.52 0 -20.16 8 -25.76 23.84 c -0.48 1.44 -2.08 2.24 -3.52 1.6 c -1.44 -0.48 -2.24 -2.08 -1.6 -3.36 c 6.4 -18.24 16.8 -27.52 30.88 -27.52 c 18.4 0 26.88 23.36 27.84 38.24 c 0.16 1.44 -0.96 2.88 -2.56 2.88 z" fill="#1d2d35"></path>
               <path className="icon" d="M 34.88 51.2 c -9.28 0 -18.24 -4 -24.48 -11.04 c -0.8 -0.64 -0.96 -1.76 -0.64 -2.72 c 1.76 -4.64 6.4 -15.36 12.32 -15.52 c 3.04 0 5.28 2.4 7.04 4.32 c 0.48 0.64 1.28 1.44 1.92 1.92 c 1.44 -1.44 4.16 -5.92 6.24 -10.56 c 0.64 -1.44 2.24 -1.92 3.68 -1.28 s 1.92 2.24 1.28 3.68 c -6.4 13.92 -9.76 13.92 -11.04 13.92 c -2.56 0 -4.32 -1.92 -6.08 -3.84 c -0.8 -0.8 -2.4 -2.56 -3.04 -2.56 c -1.44 0.16 -4.48 5.12 -6.56 10.4 c 5.28 5.28 12.48 8.16 19.84 8 c 6.4 0 17.76 -1.12 22.4 -8 c -1.28 -13.76 -8.8 -32.32 -22.4 -32.32 c -1.44 0 -2.72 -1.28 -2.72 -2.72 s 1.28 -2.88 2.88 -2.88 c 18.4 0 26.88 23.36 27.84 38.24 c 0 0.48 -0.16 1.12 -0.32 1.6 c -4.32 7.36 -14.08 11.52 -27.52 11.52 c -0.16 -0.16 -0.48 -0.16 -0.64 -0.16 z" fill="#1d2d35"></path>
             </svg>
@@ -141,12 +141,12 @@ class SessionForm extends React.Component {
                 </div>
               </div>
               {this.renderLowerSignup()}
-              <div className="login-form-errors">
-                {this.renderErrors()}
-              </div>
             </div>
           </form>
         </div>
+          <div className="login-form-errors">
+            {this.renderErrors()}
+          </div>
         <div className="login-bottom">        
         </div>
       </div>
