b6b5ab14440bc55164f703994d4cb71913788957
droid4alex <droid4alex@gmail.com>
2021-02-17 12:49:06 -0800

    Finished User auth: CSS

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -71,7 +71,7 @@ class SessionForm extends React.Component {
   demoButton(){
     if (this.props.formType === "Login"){
       return (
-      <button type="submit" className="session_submit"
+      <button type="submit" className="demo-button"
         onClick={() => this.setState({ email: "demo@gmail.com", password: '123456' })}>
         Try a Demo
       </button>
@@ -88,72 +88,65 @@ class SessionForm extends React.Component {
 
     if (this.props.formType === "Login") {
       text = "Don't have an account?"
-      link = <Link to="/signup">Create account</Link>
+      link = <Link to="/signup" className="lowersignup-link">Create account</Link>
     } else {
       text = "Already have an account?"
-      link = <Link to="/login">Sign in</Link>
+      link = <Link to="/login" className="lowersignup-link">Sign in</Link>
     };
 
     return (
       <div className="lowersignup">
-        <div className="lowersignup-text">
-          {text}
-        </div>
-        <div className="lowersignup-link">
-          {link}
-        </div>
+        <div className="lowersignup-text">{text} {link}</div>
       </div>
     )
   }
 
   render() {
     return (
-      <div className="login-form-container">
-        <a className="top-nav__logo" href="/" aria-label="Basecamp">
-          <svg viewBox="0 0 149.8 41.4" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="basecamp-logo-title">
-            <title id="basecamp-logo-title">Basecamp logo</title>
-            <path className="icon" d="m37.9 25.7c-.9 0-1.6-.7-1.7-1.6-.6-8.6-5.3-20.7-14-20.7-7.2 0-12.6 5-16.1 14.9-.3.9-1.3 1.4-2.2 1-.9-.3-1.4-1.3-1-2.1 4-11.4 10.5-17.2 19.3-17.2 11.5 0 16.8 14.6 17.4 23.9.1.9-.6 1.8-1.6 1.8z" fill="#1d2d35"></path>
-            <path className="icon" d="m21.8 32c-5.8 0-11.4-2.5-15.3-6.9-.5-.4-.6-1.1-.4-1.7 1.1-2.9 4-9.6 7.7-9.7 1.9 0 3.3 1.5 4.4 2.7.3.4.8.9 1.2 1.2.9-.9 2.6-3.7 3.9-6.6.4-.9 1.4-1.2 2.3-.8s1.2 1.4.8 2.3c-4 8.7-6.1 8.7-6.9 8.7-1.6 0-2.7-1.2-3.8-2.4-.5-.5-1.5-1.6-1.9-1.6-.9.1-2.8 3.2-4.1 6.5 3.3 3.3 7.8 5.1 12.4 5 4 0 11.1-.7 14-5-.8-8.6-5.5-20.2-14-20.2-.9 0-1.7-.8-1.7-1.7s.8-1.8 1.8-1.8c11.5 0 16.8 14.6 17.4 23.9 0 .3-.1.7-.2 1-2.7 4.6-8.8 7.2-17.2 7.2-.1-.1-.3-.1-.4-.1z" fill="#1d2d35"></path>
-            <g className="name" fill="#1d2d35">
-              <path d="m46.8 8.5h6.1c3.4 0 5.2 1.4 5.2 4v.1c.1 1.6-1 3-2.5 3.3 1.9.2 3.2 1.8 3.1 3.7v.1c0 2.9-1.9 4.4-5.5 4.4h-6.4zm5.5 6.4c1.7 0 2.4-.6 2.4-2v-.1c0-1.3-.7-1.9-2.4-1.9h-2.1v4zm.4 6.7c1.7 0 2.5-.8 2.5-2.2v-.1c0-1.4-.8-2.2-2.7-2.2h-2.2v4.5z"></path>
-              <path d="m59.6 20.9c0-2.6 2.4-3.6 5.9-3.6h1.3v-.5c0-1.4-.4-2.1-1.8-2.1-.9-.1-1.8.5-1.9 1.5v.1h-3c.2-2.7 2.3-3.9 5.1-3.9s4.8 1.1 4.8 4.2v7.4h-3.1v-1.3c-.8 1.1-2.1 1.7-3.4 1.6-2.2 0-3.9-1-3.9-3.4zm7.2-.8v-1h-1.2c-1.8 0-2.9.4-2.9 1.6 0 .8.5 1.4 1.7 1.4 1.3 0 2.4-.8 2.4-2z"></path>
-              <path d="m71.2 20.5h2.9c.1 1 .6 1.6 2 1.6s1.8-.5 1.8-1.2-.7-1.1-2.3-1.4c-3-.5-4.2-1.3-4.2-3.6s2.1-3.5 4.5-3.5c2.5 0 4.4.9 4.7 3.5h-2.9c-.2-.9-.7-1.4-1.8-1.4s-1.6.5-1.6 1.2.5 1 2.2 1.2c2.8.4 4.4 1.1 4.4 3.6s-1.8 3.8-4.8 3.8-4.8-1.4-4.9-3.8z"></path>
-              <path d="m81.8 18.5v-.2c-.2-3.1 2.2-5.8 5.4-5.9h.5c2.9 0 5.6 1.7 5.6 5.8v.9h-8.3c.1 1.9 1.1 3 2.8 3 1.5 0 2.2-.6 2.4-1.6h3c-.4 2.5-2.3 3.8-5.4 3.8-3.5 0-6-2.2-6-5.8zm8.3-1.4c-.1-1.7-1-2.6-2.5-2.6-1.4 0-2.6 1.1-2.6 2.6z"></path>
-              <path d="m94 18.5v-.2c-.1-3.1 2.3-5.8 5.4-5.9h.5c2.6 0 5.1 1.1 5.3 4.4h-3c-.1-1.1-1.1-2-2.2-1.9-1.6 0-2.7 1.2-2.7 3.4v.2c0 2.3 1 3.5 2.8 3.5 1.3.1 2.4-.9 2.4-2.2h2.8c-.2 2.7-2.1 4.6-5.4 4.6s-5.9-2.2-5.9-5.9z"></path>
-              <path d="m106.1 20.9c0-2.6 2.4-3.6 5.9-3.6h1.3v-.5c0-1.4-.4-2.1-1.8-2.1-.9-.1-1.8.5-1.9 1.5v.2h-3c.2-2.7 2.3-3.9 5.1-3.9s4.8 1.1 4.8 4.2v7.3h-3.1v-1.3c-.8 1.1-2.1 1.7-3.4 1.6-2.2 0-3.9-1-3.9-3.4zm7.1-.8v-1h-1.2c-1.8 0-2.9.4-2.9 1.6 0 .8.5 1.4 1.7 1.4 1.4 0 2.4-.8 2.4-2z"></path>
-              <path d="m118.5 12.7h3.2v1.7c.7-1.3 2-2 3.5-2 1.4-.1 2.7.7 3.2 2 .9-1.3 2.3-2 3.8-2 2.1 0 3.8 1.3 3.8 4.4v7.2h-3.1v-6.9c0-1.5-.6-2.1-1.8-2.1-1.2-.1-2.2.9-2.2 2v.3 6.7h-3.1v-6.9c0-1.5-.7-2.1-1.8-2.1-1.2-.1-2.1.9-2.2 2v.3 6.7h-3.2z"></path>
-              <path d="m138.1 12.7h3.1v1.8c.8-1.3 2.2-2 3.6-2 2.8 0 5 2.1 5 5.8v.2c0 3.8-2.1 5.9-5 5.9-1.5.1-2.9-.7-3.6-2v5.6h-3.1zm8.5 5.7v-.2c0-2.3-1.1-3.4-2.7-3.4s-2.8 1.1-2.8 3.4v.2c0 2.3 1.1 3.4 2.8 3.4s2.7-1.2 2.7-3.4z"></path>
-            </g>
-          </svg>
-        </a>
-        <form onSubmit={this.handleSubmit} className="login-form-box">
-          <div className="login-form">
-            <br />
-            <label>Email:
-              <input type="text"
-                value={this.state.email}
-                onChange={this.update('email')}
-                className="login-input"
-              />
-            </label>
-            <br />
-            <label>Password:
-              <input type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                className="login-input"
-              />
-            </label>
-            {this.renderSignup()}
-            <div className="login-form-errors">
-              {this.renderErrors()}
+      <div className="login-body">
+        <div className="login-logo">
+          <a className="login-nav__logo" href="/" aria-label="Basecamp">
+            <svg viewBox="0 0 100 60" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="basecamp-logo-title">
+              <title id="basecamp-logo-title">Democamp logo</title>
+              <path className="icon" d="M 60.64 41.12 c -1.44 0 -2.56 -1.12 -2.72 -2.56 c -0.96 -13.76 -8.48 -33.12 -22.4 -33.12 c -11.52 0 -20.16 8 -25.76 23.84 c -0.48 1.44 -2.08 2.24 -3.52 1.6 c -1.44 -0.48 -2.24 -2.08 -1.6 -3.36 c 6.4 -18.24 16.8 -27.52 30.88 -27.52 c 18.4 0 26.88 23.36 27.84 38.24 c 0.16 1.44 -0.96 2.88 -2.56 2.88 z" fill="#1d2d35"></path>
+              <path className="icon" d="M 34.88 51.2 c -9.28 0 -18.24 -4 -24.48 -11.04 c -0.8 -0.64 -0.96 -1.76 -0.64 -2.72 c 1.76 -4.64 6.4 -15.36 12.32 -15.52 c 3.04 0 5.28 2.4 7.04 4.32 c 0.48 0.64 1.28 1.44 1.92 1.92 c 1.44 -1.44 4.16 -5.92 6.24 -10.56 c 0.64 -1.44 2.24 -1.92 3.68 -1.28 s 1.92 2.24 1.28 3.68 c -6.4 13.92 -9.76 13.92 -11.04 13.92 c -2.56 0 -4.32 -1.92 -6.08 -3.84 c -0.8 -0.8 -2.4 -2.56 -3.04 -2.56 c -1.44 0.16 -4.48 5.12 -6.56 10.4 c 5.28 5.28 12.48 8.16 19.84 8 c 6.4 0 17.76 -1.12 22.4 -8 c -1.28 -13.76 -8.8 -32.32 -22.4 -32.32 c -1.44 0 -2.72 -1.28 -2.72 -2.72 s 1.28 -2.88 2.88 -2.88 c 18.4 0 26.88 23.36 27.84 38.24 c 0 0.48 -0.16 1.12 -0.32 1.6 c -4.32 7.36 -14.08 11.52 -27.52 11.52 c -0.16 -0.16 -0.48 -0.16 -0.64 -0.16 z" fill="#1d2d35"></path>
+            </svg>
+          </a>
+        </div>
+        <div className="login-form-container">
+          <form onSubmit={this.handleSubmit} className="login-form-box">
+            <div className="login-field">
+              <label>Email
+                <input type="text"
+                  value={this.state.email}
+                  onChange={this.update('email')}
+                  className="login-input"
+                />
+              </label>
+              <br />
+              <label>Password:
+                <input type="password"
+                  value={this.state.password}
+                  onChange={this.update('password')}
+                  className="login-input"
+                />
+              </label>
+              {this.renderSignup()}
+              <div className="login-demo-div">
+                <div className="login-button-div">
+                  <input className="button-login" type="submit" value={this.props.formType} />
+                </div>
+                <div className="demo-button-div">
+                  {this.demoButton()}
+                </div>
+              </div>
+              {this.renderLowerSignup()}
+              <div className="login-form-errors">
+                {this.renderErrors()}
+              </div>
             </div>
-            <input className="session-submit" type="submit" value={this.props.formType} />
-            {this.demoButton()}
-            <br />
-            {this.renderLowerSignup()}
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     );
   }
