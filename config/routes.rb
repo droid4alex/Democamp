20e92b622333e57507682f81f3e88fdf78e6ab41
droid4alex <droid4alex@gmail.com>
2021-02-26 04:57:29 -0800

    Updates routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
     
+  root to: 'static_pages#root'
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
   end
 
-  root to: 'static_pages#root'
+  resources :projects, only: [:show, :create, :update, :destroy] do
+    resources :todos, only: [:index]
+    resources :messages, only: [:index]
+    # resources :teams, only: [:show, :create, :update]
+  end
+
+  resources :todos, only: [:show, :create, :update, :destroy]
+
+  resources :messages, only: [:show, :create, :update, :destroy] do
+    resources :replies, only: [:index]
+  end
+
+  resources :replies, only: [:create, :update, :destroy]
 
 end
