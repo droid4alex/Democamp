59ea4a764ca27d263337bbb83e8bbb123905bdf6
droid4alex <droid4alex@gmail.com>
2021-04-01 18:05:32 -0700

    Updates controllers

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,49 @@
 class MessagesController < ApplicationController
+ def index
+  @project = Project.find(params[:project_id])
+  if @project
+    @messages = @project.messages
+    render 'api/messages/index'
+  else
+    @errors = ['Project not found']
+    render 'api/messages/index', status: 404
+  end
+ end
+
+  def show
+    @message = Message.find(params[:id])
+    if @message
+      render 'api/messages/show'
+    else
+      render 'api/messages/show', status: 404
+    end
+  end
+
+  def create
+    @message = Message.create(message_params)
+    if @message.save
+      render 'api/messages/show'
+    else
+      @errors = @message.errors.full_messages
+      render 'api/messages/show', status: 422
+    end
+  end
+
+  def update
+    @message = Message.find(params[:id]) 
+    if @message
+      if @message.update(message_params)
+        render 'api/messages/show'
+      else
+        @errors = @message.errors.full_messages
+        render 'api/messages/show', status: 422
+      end
+    else
+      render 'api/messages/show', status: 404
+    end
+  end
+
+  def message_params
+    params.require(:message).permit(:title, :body, :author_id, :project_id)
+  end
 end
--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,2 +1,49 @@
 class TodosController < ApplicationController
+  def index
+  @project = Project.find(params[:project_id])
+    if @project
+      @todos = @project.todos
+      render 'api/todos/index'
+    else
+      render 'api/todos/index', status: 404
+    end
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    if @todo
+      render 'api/todos/show'
+    else
+      render 'api/todos/show', status: 404
+    end
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    if @todo.save
+      render 'api/todos/show'
+    else
+      @errors = @todo.errors.full_messages
+      render 'api/todos/show', status: 422
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo
+      if @todo.update(todo_params)
+        render 'api/todos/show'
+      else
+        @errors = @todo.errors.full_messages
+        render 'api/todos/show', status: 422
+      end
+    else
+      render 'api/todos/show', status: 404
+    end
+  end
+
+  def todo_params
+    params.require(:todo).permit(:due_date, :status, :title, :assignee_id, :project_id)
+  end
+
 end
