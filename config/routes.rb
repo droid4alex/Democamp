f8d353f9350191c8393e8e15fbe815c07d768684
droid4alex <droid4alex@gmail.com>
2021-08-03 07:14:37 -0700

    Updates projects CSS, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,39 @@
-# == Route Map
-#
-#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+# Prefix Verb   URI Pattern                                                                              Controller#Action
 #                      root GET    /                                                                                        static_pages#root
 #                 api_users POST   /api/users(.:format)                                                                     api/users#create {:format=>:json}
 #               api_session DELETE /api/session(.:format)                                                                   api/sessions#destroy {:format=>:json}
 #                           POST   /api/session(.:format)                                                                   api/sessions#create {:format=>:json}
-#             project_todos GET    /projects/:project_id/todos(.:format)                                                    todos#index
-#          project_messages GET    /projects/:project_id/messages(.:format)                                                 messages#index
-#                  projects POST   /projects(.:format)                                                                      projects#create
-#                   project GET    /projects/:id(.:format)                                                                  projects#show
-#                           PATCH  /projects/:id(.:format)                                                                  projects#update
-#                           PUT    /projects/:id(.:format)                                                                  projects#update
-#                           DELETE /projects/:id(.:format)                                                                  projects#destroy
-#                     todos POST   /todos(.:format)                                                                         todos#create
-#                      todo GET    /todos/:id(.:format)                                                                     todos#show
-#                           PATCH  /todos/:id(.:format)                                                                     todos#update
-#                           PUT    /todos/:id(.:format)                                                                     todos#update
-#                           DELETE /todos/:id(.:format)                                                                     todos#destroy
-#           message_replies GET    /messages/:message_id/replies(.:format)                                                  replies#index
-#                  messages POST   /messages(.:format)                                                                      messages#create
-#                   message GET    /messages/:id(.:format)                                                                  messages#show
-#                           PATCH  /messages/:id(.:format)                                                                  messages#update
-#                           PUT    /messages/:id(.:format)                                                                  messages#update
-#                           DELETE /messages/:id(.:format)                                                                  messages#destroy
-#                   replies POST   /replies(.:format)                                                                       replies#create
-#                     reply PATCH  /replies/:id(.:format)                                                                   replies#update
-#                           PUT    /replies/:id(.:format)                                                                   replies#update
-#                           DELETE /replies/:id(.:format)                                                                   replies#destroy
+#         api_user_projects GET    /api/users/:user_id/projects(.:format)                                                   api/projects#index {:format=>:json}           api_user_todos GET    /api/users/:user_id/todos(.:format)                                                      api/todos#index {:format=>:json}
+#                  api_user GET    /api/users/:id(.:format)                                                                 api/users#show {:format=>:json}
+#              api_projects GET    /api/projects(.:format)                                                                  api/projects#index {:format=>:json}                          POST   /api/projects(.:format)                                                                  api/projects#create {:format=>:json}
+#               api_project GET    /api/projects/:id(.:format)                                                              api/projects#show {:format=>:json}
+#                           PATCH  /api/projects/:id(.:format)                                                              api/projects#update {:format=>:json}
+#                           PUT    /api/projects/:id(.:format)                                                              api/projects#update {:format=>:json}
+#                           DELETE /api/projects/:id(.:format)                                                              api/projects#destroy {:format=>:json}
+#                 api_todos GET    /api/todos(.:format)                                                                     api/todos#index {:format=>:json}
+#                           POST   /api/todos(.:format)                                                                     api/todos#create {:format=>:json}
+#                  api_todo GET    /api/todos/:id(.:format)                                                                 api/todos#show {:format=>:json}
+#                           PATCH  /api/todos/:id(.:format)                                                                 api/todos#update {:format=>:json}
+#                           PUT    /api/todos/:id(.:format)                                                                 api/todos#update {:format=>:json}
+#                           DELETE /api/todos/:id(.:format)                                                                 api/todos#destroy {:format=>:json}
+#              api_messages GET    /api/messages(.:format)                                                                  api/messages#index {:format=>:json}                          POST   /api/messages(.:format)                                                                  api/messages#create {:format=>:json}
+#               api_message GET    /api/messages/:id(.:format)                                                              api/messages#show {:format=>:json}
+#                           PATCH  /api/messages/:id(.:format)                                                              api/messages#update {:format=>:json}
+#                           PUT    /api/messages/:id(.:format)                                                              api/messages#update {:format=>:json}
+#                           DELETE /api/messages/:id(.:format)                                                              api/messages#destroy {:format=>:json}
+#               api_replies GET    /api/replies(.:format)                                                                   api/replies#index {:format=>:json}
+#                           POST   /api/replies(.:format)                                                                   api/replies#create {:format=>:json}                api_reply GET    /api/replies/:id(.:format)                                                               api/replies#show {:format=>:json}
+#                           PATCH  /api/replies/:id(.:format)                                                               api/replies#update {:format=>:json}                          PUT    /api/replies/:id(.:format)                                                               api/replies#update {:format=>:json}                          DELETE /api/replies/:id(.:format)                                                               api/replies#destroy {:format=>:json}
+#                 api_teams POST   /api/teams(.:format)                                                                     api/teams#create {:format=>:json}
+#                  api_team GET    /api/teams/:id(.:format)                                                                 api/teams#show {:format=>:json}
+#                           PATCH  /api/teams/:id(.:format)                                                                 api/teams#update {:format=>:json}
+#                           PUT    /api/teams/:id(.:format)                                                                 api/teams#update {:format=>:json}
+#                           DELETE /api/teams/:id(.:format)                                                                 api/teams#destroy {:format=>:json}
 #        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
-# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
-#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show       rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
 # update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
 #      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
 
 Rails.application.routes.draw do
     
@@ -43,23 +45,24 @@ Rails.application.routes.draw do
       resources :projects, only: [:index]
       resources :todos, only: [:index]
     end
-    resources :projects, only: [:index]
-    resources :todos, only: [:index]
-    resources :messages, only: [:index]
+    resources :projects, only: [:index, :show, :create, :update, :destroy]
+    resources :todos, only: [:index, :show, :create, :update, :destroy]
+    resources :messages, only: [:index, :show, :create, :update, :destroy]
+    resources :replies, only: [:index, :show, :create, :update, :destroy]
+    resources :teams, only: [:show, :create, :update, :destroy]
   
-    resources :projects, only: [:show, :create, :update, :destroy] do
-      resources :todos, only: [:index] #if not nested, remember to send ID through frontend + controoler
-      resources :messages, only: [:index]
-      # resources :teams, only: [:show, :create, :update]
-    end
+    #not nested, send project ID through frontend + controller
+    # resources :projects, only: [:show, :create, :update, :destroy] do
+    #   resources :todos, only: [:index]
+    #   resources :messages, only: [:index]
+    #   # resources :teams, only: [:show, :create, :update]
+    # end 
+      
+    #not nested, send project ID and message ID through frontend + controller
+    # resources :messages, only: [:show, :create, :update, :destroy] do
+    #   resources :replies, only: [:index]
+    # end 
   
-    resources :todos, only: [:show, :create, :update, :destroy]
-  
-    resources :messages, only: [:show, :create, :update, :destroy] do
-      resources :replies, only: [:index]
-    end
-  
-    resources :replies, only: [:create, :update, :destroy]
   end
 
 
--- frontend/components/projects/projects.jsx
+++ frontend/components/projects/projects.jsx
@@ -27,13 +27,13 @@ class Projects extends React.Component {
                 </p>
                 <p className="card__description__icons flush" title="" data-role="content_filter_text">
                   <Link to={values[i].id + "/messages/"}>
-                    <svg className="bc-tools__icon" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#1b6ac9" r="20" /><path d="m28.0459331 10h-15.9434943c-1.0512194 0-2.1024388.7246377-2.1024388 1.9927536v19.7463768l4.0296744-4.1666666h14.0162587c1.0512195 0 1.9272356-.9057971 1.9272356-1.9927537v-13.5869565c.1752033-.9057971-.5256097-1.8115942-1.5768291-1.9927536 0 0-.1752032 0-.3504065 0zm-1.5768291 13.5869565h-12.7898361v-1.4492753h12.9650394v1.4492753zm0-4.1666666h-12.7898361v-1.268116h12.9650394v1.268116zm0-3.9855073h-12.7898361v-1.2681159h12.9650394v1.2681159z" fill="#fff" /></g></svg>
+                    <svg className="bc-tools__icon__project" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#1b6ac9" r="20" /><path d="m28.0459331 10h-15.9434943c-1.0512194 0-2.1024388.7246377-2.1024388 1.9927536v19.7463768l4.0296744-4.1666666h14.0162587c1.0512195 0 1.9272356-.9057971 1.9272356-1.9927537v-13.5869565c.1752033-.9057971-.5256097-1.8115942-1.5768291-1.9927536 0 0-.1752032 0-.3504065 0zm-1.5768291 13.5869565h-12.7898361v-1.4492753h12.9650394v1.4492753zm0-4.1666666h-12.7898361v-1.268116h12.9650394v1.268116zm0-3.9855073h-12.7898361v-1.2681159h12.9650394v1.2681159z" fill="#fff" /></g></svg>
                   </Link>
                   <Link to={values[i].id + "/todos/"}>
-                    <svg className="bc-tools__icon" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#3cb371" r="20" /><path d="m17.4 23.4758621-6.8714286-6.6344828-3.5285714 3.2275862 6.8714286 6.6344828 3.5285714 3.2275862 15.6-14.7034483-3.5285714-3.2275862z" fill="#fff" /></g></svg>
+                    <svg className="bc-tools__icon__project" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#3cb371" r="20" /><path d="m17.4 23.4758621-6.8714286-6.6344828-3.5285714 3.2275862 6.8714286 6.6344828 3.5285714 3.2275862 15.6-14.7034483-3.5285714-3.2275862z" fill="#fff" /></g></svg>
                   </Link>
                   <Link to={values[i].id + "/schedule/"}>
-                    <svg className="bc-tools__icon" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#ff2d55" r="20" /><path d="m25.5555556 21.2692308h-5.5555556v5.576923h5.5555556zm-1.1111112-12.2692308v2.2307692h-8.8888888v-2.2307692h-2.2222223v2.2307692h-1.1111111c-1.2698412 0-2.2222222.956044-2.2222222 2.2307693v15.6153846c0 1.2747253.952381 2.2307692 2.2222222 2.2307692h15.5555556c1.2698412 0 2.2222222-.9560439 2.2222222-2.2307692v-15.6153846c0-1.2747253-.952381-2.2307693-2.2222222-2.2307693h-1.1111111v-2.2307692zm3.3333334 20.0769231h-15.5555556v-12.2692308h15.5555556z" fill="#fff" /></g></svg>
+                    <svg className="bc-tools__icon__project" height="40" viewBox="0 0 40 40" width="40" xmlns="http://www.w3.org/2000/svg"><g fill="none"><circle cx="20" cy="20" fill="#ff2d55" r="20" /><path d="m25.5555556 21.2692308h-5.5555556v5.576923h5.5555556zm-1.1111112-12.2692308v2.2307692h-8.8888888v-2.2307692h-2.2222223v2.2307692h-1.1111111c-1.2698412 0-2.2222222.956044-2.2222222 2.2307693v15.6153846c0 1.2747253.952381 2.2307692 2.2222222 2.2307692h15.5555556c1.2698412 0 2.2222222-.9560439 2.2222222-2.2307692v-15.6153846c0-1.2747253-.952381-2.2307693-2.2222222-2.2307693h-1.1111111v-2.2307692zm3.3333334 20.0769231h-15.5555556v-12.2692308h15.5555556z" fill="#fff" /></g></svg>
                   </Link>
                 </p>
               </div>
@@ -81,7 +81,16 @@ class Projects extends React.Component {
             {this.getProjects()}
             <aside className="project-index__toolbar project-index__toolbar--new hide-from-clients" role="presentation" data-behavior="hide_when_content_filter_active">
               <span className="options-menu options-menu--add-project" data-purpose="topic" data-behavior="expandable render_new_project_form_on_expand reveal_on_expand">
-                <button name="button" type="button" title="Start a new project…" className="options-menu__expansion-toggle btn btn--small btn--with-icon btn--add-icon" data-behavior="toggle_expansion_on_click">&nbsp; 	&nbsp; New</button>
+                <div className="RowStructure-leftChildren">
+                  <div className="AddItemSquircleRow-leftElements">
+                    <div className="AddItemSquircleRow-box">
+                      <svg className="Icon AddItemSquircleRow-plusIcon PlusIcon" focusable="false" viewBox="0 0 32 32">
+                        <path d="M26,14h-8V6c0-1.1-0.9-2-2-2l0,0c-1.1,0-2,0.9-2,2v8H6c-1.1,0-2,0.9-2,2l0,0c0,1.1,0.9,2,2,2h8v8c0,1.1,0.9,2,2,2l0,0c1.1,0,2-0.9,2-2v-8h8c1.1,0,2-0.9,2-2l0,0C28,14.9,27.1,14,26,14z">
+                        </path>
+                      </svg>
+                    </div>New Project
+                  </div>
+                </div>
               </span>
             </aside>
           </div>
