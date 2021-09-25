c414a44f97a0f33955e41fc75723df74ddb19860
droid4alex <droid4alex@gmail.com>
2021-09-25 05:31:11 -0700

    Updates projects

--- frontend/components/projects/projects.jsx
+++ frontend/components/projects/projects.jsx
@@ -58,46 +58,6 @@ class Projects extends React.Component {
 
   setProjectState(project){
   }
-  // showModal = () => {
-  //   this.setState({ show: true });
-  // };
-
-  // hideModal = () => {
-  //   this.setState({ show: false });
-  // };
-  
-//   projectModal(project) {
-//     const[show, setShow] = useState(false);
-
-//   return (
-//     <>
-//       <Button variant="primary" onClick={this.handleShow}>
-//         Launch static backdrop modal
-//       </Button>
-
-//       <Modal
-//         show={show}
-//         onHide={this.handleClose}
-//         backdrop="static"
-//         keyboard={false}
-//       >
-//         <Modal.Header closeButton>
-//           <Modal.Title>Modal title - Project {project}</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           I will not close if you click outside me. Don't even try to press
-//           escape key.
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={this.handleClose}>
-//             Close
-//           </Button>
-//           <Button variant="primary">Understood</Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </>
-//   );
-// }
 
   
 
